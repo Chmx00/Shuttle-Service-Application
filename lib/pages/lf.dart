53fe074e2f73574a69c77e1065f48d6eba3b72cb
Darshana Madhushan <87925393+daraya99@users.noreply.github.com>
2023-09-10 21:01:37 +0530

    submit lost & found column

--- lib/pages/lf.dart
+++ lib/pages/lf.dart
@@ -96,6 +96,69 @@ class LostAndFoundPage extends StatelessWidget {
                           ],
                         ),
                         SizedBox(height: 20),
+                        Column(
+                        children: [
+                          Container(
+                             width: double.infinity,
+                             height: 86,
+                             decoration: BoxDecoration(
+                             color: const Color.fromARGB(255, 123, 196, 255),
+                             borderRadius:BorderRadius.circular(22)
+                            ),
+                            child: Row(
+                                  children: [
+                                    Container(
+                                      alignment: Alignment.centerRight,
+                                      child: Padding(
+                                          padding: EdgeInsets.only(left: 20.0),
+                                          child: ElevatedButton(
+                                            style: ButtonStyle(
+                                             backgroundColor: MaterialStateProperty.all(const Color.fromARGB(255, 123, 196, 255)), 
+                                            ),
+                                            onPressed: () { 
+                                              Navigator.of(context).push(MaterialPageRoute(
+                                                builder: (_) {
+                                                  return SubmitLost();
+                                                }
+                                              ));
+                                             },
+                                            child: Text(
+                                              'Sumbit Lost/\nFound Items',
+                                              style: TextStyle(
+                                                color: Colors.black,
+                                                fontSize: 22,
+                                              ),
+                                            ),
+                                          )),
+                                    ),
+                                    const Spacer(),
+                                    Row(
+                                      children: [
+                                        Container(
+                                          alignment: Alignment.centerRight,
+                                          child: Padding(
+                                            padding:
+                                                EdgeInsets.only(right: 10.0),
+                                            child: ClipRRect(
+                                              borderRadius:
+                                                  BorderRadius.circular(20.0),
+                                              child: Image.asset(
+                                                'assets/lost.PNG',
+                                                height: 60,
+                                                width: 60,
+                                              ),
+                                            ),
+                                          ),
+                                        )
+                                      ],
+                                    )
+                                  ],
+                                )
+                          ),
+                          
+                        ],
+                      ),
+                      SizedBox(height: 20),
           ],
           ),
        ),
